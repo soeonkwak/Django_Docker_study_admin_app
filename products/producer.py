6006b0066f28d3a3a4116b9bf4b948ec6ac873b0
Soeon <rhkrthdjs@naver.com>
2022-11-01 02:11:24 +0900

    rabbitMQ 메세징3

--- products/producer.py
+++ products/producer.py
@@ -1,4 +1,4 @@
-import pika  # 이벤트를 보내는데 필요한 패키지
+import pika, json  # 이벤트를 보내는데 필요한 패키지
 
 # URL주소는 CloudAMQP에서 계정생성하고 부여 받은 거.
 params = pika.URLParameters('amqps://uuyqugbb:koSGNC-lwhlAm83l-uCIDy5g2SGJ5yBj@dingo.rmq.cloudamqp.com/uuyqugbb')
@@ -11,6 +11,7 @@ channel = connection.channel()
 
 
 # 함수 생성 -> 이 함수 사용해서 products.views 에서 메세징 할 수 있음.
-def publish():
+def publish(method, body):
+    properties = pika.BasicProperties(method)
     # routing_key: 이벤트를 보내고 싶은 큐? -> admin app에서 뭔가 이벤트가 발생하면 main app으로 메세지 보낼 거니까 'main'
-    channel.basic_publish(exchange='', routing_key='main', body='hello main')
+    channel.basic_publish(exchange='', routing_key='main', body=json.dumps(body), properties=properties)
--- products/views.py
+++ products/views.py
@@ -12,16 +12,17 @@ from .serializers import ProductSerializer
 # Create your views here.
 
 class ProductViewSet(viewsets.ViewSet):
+
     def list(self, request):
         products = Product.objects.all()
         serializer = ProductSerializer(products, many=True)
-        publish()  # producer.py
         return Response(serializer.data)
 
     def create(self, request):
         serializer = ProductSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
+        publish('product_created', serializer.data)  # producer.py
         return Response(serializer.date, status=status.HTTP_201_CREATED)
 
     def retrieve(self, request, pk=None):
@@ -35,15 +36,17 @@ class ProductViewSet(viewsets.ViewSet):
                                        data=request.data)  # serializer 구해야 되는데 ProductSerializer랑 같음. arg1 : 우리가 원래 갖고 있는 데이터, arg2: request로 부터 받은 update 될 값
         serializer.is_valid(raise_exception=True)  # 위랑 마찬가지로 serializer 값 검증?
         serializer.save()  # 검증 통과하면 save
+        publish('product_updated', serializer.data)
         return Response(serializer.date, status=status.HTTP_202_ACCEPTED)
 
     def destroy(self, request, pk=None):
         product = Product.objects.get(id=pk)
         product.delete()
+        publish('product_deleted', pk)
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-class UserAPIView(APIView): #우리는 .. rest_framework에서 제공하는 APIView 사용할 거 ..
+class UserAPIView(APIView):  # 우리는 .. rest_framework에서 제공하는 APIView 사용할 거 ..
     def get(self, _):
         users = User.objects.all()
         user = random.choice(users)
